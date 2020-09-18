1484aaa00d9a927f069e38b2f31cdf03ba44a5e3
JustLiving <JustinTLivingston@gmail.com>
2020-09-18 11:09:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Blackjack Project
+
+## Topics
+1.
+2.
+3.
+
+## Lessons Learned
+*
+*
+*
+
+## How to run
+1.
+2.
+3.
---
+++ src/com/skilldistillery/common/cards/Deck.java
@@ -0,0 +1,39 @@
+package com.skilldistillery.common.cards;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+	// Create a class that holds a List of cards.
+	private List<Card> cards; // List of cards
+
+	//Intializing list with 52 cards.
+	public Deck() {
+		cards = createDeck();
+	}
+
+	//Method that passes the list of cards and adds to deck.
+	private List<Card> createDeck() {
+		List<Card> deck = new ArrayList<>(52);
+		for (Suit s : Suit.values()) {
+			for (Rank r : Rank.values()) {
+				deck.add(new Card(r, s));
+			}
+		}
+		return deck;
+	}
+
+	public int checkDeckSize() {
+		return cards.size();
+	}
+
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+
+	public Card dealCard() {
+		return cards.remove(0);
+	}
+
+}
